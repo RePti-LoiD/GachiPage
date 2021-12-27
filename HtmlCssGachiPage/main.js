4643636dce9de8eb35cd4a35df7f92ac05fdb893
Сергей <95445727+RePti-LoiD@users.noreply.github.com>
2021-12-27 23:53:46 +0300

    Update main.js

--- HtmlCssGachiPage/main.js
+++ HtmlCssGachiPage/main.js
@@ -1,6 +1,6 @@
 $('.open-popup').click(function(e){
     e.preventDefault();
-    document.getElementById('popup-text').innerHTML = "С наступающим ! Понимаю бокал за вас !";
+    document.getElementById('popup-text').innerHTML = "С наступающим ! Поднимаю бокал за вас !";
     document.getElementById('popup-img').src = 'celebrate.jpg';
     $('.popup-bg').fadeIn(600);
 });
