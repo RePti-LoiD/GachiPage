559197c3dabd3ea43a9c15913516536850050725
Сергей <95445727+RePti-LoiD@users.noreply.github.com>
2021-12-27 23:50:37 +0300

    This Finally GachiPage Version!!
    This Finally GachiPage Version!! 
    But, maybe we'll be working on this in future!

---
+++ HtmlCssGachiPage/main.js
@@ -0,0 +1,9 @@
+$('.open-popup').click(function(e){
+    e.preventDefault();
+    document.getElementById('popup-text').innerHTML = "С наступающим ! Понимаю бокал за вас !";
+    document.getElementById('popup-img').src = 'celebrate.jpg';
+    $('.popup-bg').fadeIn(600);
+});
+$('.close-popup').click(function(){
+    $('.popup-bg').fadeOut(600);
+});
